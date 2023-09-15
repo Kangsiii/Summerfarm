1bed2711de67825ef8661d34a7bcabb51a8eca04
Inhye Kim <kih0245@naver.com>
2023-09-15 18:12:48 +0900

    고정배너

--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -2,13 +2,22 @@ import React from 'react';
 // import { Menu } from 'antd';
 // import { Link } from 'react-router-dom';
 import './css/RightSidebar.css'; // Navbar 스타일 파일
+import Button from '@mui/material/Button';
+import { MenuItem, MenuList, Paper, Stack } from '@mui/material';
 
 const RightSidebar = () => {
   return (
-    <div className="right-sidebar">      
-      <p>.</p>
-    </div>
+    <Stack direction="row" spacing={2}>
+      <Paper className="right-sidebar">
+        <MenuList>
+          <MenuItem>공지사항</MenuItem>
+          <MenuItem>최근 본 게시글</MenuItem>
+          <MenuItem>고객센터</MenuItem>
+        </MenuList>
+      </Paper>
+    </Stack>
   );
 }
+
 
 export default RightSidebar;
