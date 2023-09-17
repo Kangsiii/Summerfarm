200d9ebe608fc613011cab367af04d468ad7b00f
kangsiii <lks405jj@naver.com>
2023-09-18 08:09:04 +0900

    로그인 한 사용자만이 일자리/직거래/소통Farm볼수있게함

--- src/pages/BuyFarm.js
+++ src/pages/BuyFarm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BuyBoardList from './buy/BuyBoardList';
 import ProduceForm from './buy/ProduceForm';
 import BuyPostDetail from './buy/BuyPostDetail'; // BuyPostDetail 컴포넌트를 임포트
@@ -11,6 +11,16 @@ function BuyFarm() {
   const handleScreenChange = (screen) => {
     setCurrentScreen(screen);
   };
+  useEffect(() => {
+    // 로컬 스토리지에서 userID 가져오기
+    const userID = localStorage.getItem('userID');
+
+    // 만약 userID가 없다면 페이지 리디렉트 및 알림 메시지 표시
+    if (!userID) {
+      window.alert('로그인한 사용자만 볼 수 있습니다.');
+      window.location.href = '/'; // 홈 화면으로 리디렉트
+    }
+  }, []);
 
   return (
     <div>
